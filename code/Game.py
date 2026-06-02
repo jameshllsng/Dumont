9b30067ac321769932bdce414dde92c44d50e902
Luís (James) Miguel <lmsandrincardoso@gmail.com>
2026-06-02 14:29:12 -0300

    creating python package folder, importing files and coding from staruml to vscode and changing codes from main.py to game.py

---
+++ code/Game.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#Importing pygame bib
+import pygame
+
+class Game:
+    def __init__(self):
+        self.window = None
+
+    def run(self, ):
+        print('Setup start')
+        pygame.init()
+        window = pygame.display.set_mode(size=(600, 480))
+        print('Setup end')
+
+        print('Loop start')
+        while True:
+        #Check for all events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit() # Close window
+                    quit() # End pygame
+    
