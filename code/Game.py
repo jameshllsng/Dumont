b2f8bb0b76c798c02e5bca554679dab9415eb777
Luís (James) Miguel <lmsandrincardoso@gmail.com>
2026-06-03 11:33:34 -0300

    adding const,py with the image width and height, adjusting background image

--- code/Game.py
+++ code/Game.py
@@ -4,12 +4,13 @@
 #Importing pygame bib
 import pygame
 
+from code.Const import WIN_WIDTH, WIN_HEIGHT
 from code.Menu import Menu
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 480))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self):
         while True:
