5f0c1a35b6785e929acc4a5d2a85812dd337797d
Luís (James) Miguel <lmsandrincardoso@gmail.com>
2026-06-08 10:42:07 -0300

    added parallax effect and level select

--- code/EntityFactory.py
+++ code/EntityFactory.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-class EntityFactory:
-    def __init__(self):
-        pass
+from code.Background import Background
+from code.Const import WIN_WIDTH
 
-    def get_entity(self, entity_type):
-        pass
+
+class EntityFactory:
+
+
+    @staticmethod
+    def get_entity(entity_name: str, position=(0, 0)):
+        match entity_name:
+            case 'Level1Bg':
+                list_bg = []
+                for i in range(6):
+                    list_bg.append(Background(f'Level1Bg{i}', (0, 0)))
+                    list_bg.append(Background(f'Level1Bg{i}', (WIN_WIDTH, 0)))
+                return list_bg
--- code/Game.py
+++ code/Game.py
@@ -4,7 +4,8 @@
 #Importing pygame bib
 import pygame
 
-from code.Const import WIN_WIDTH, WIN_HEIGHT
+from code.Level import Level
+from code.Const import MENU_OPTION, WIN_WIDTH, WIN_HEIGHT
 from code.Menu import Menu
 
 class Game:
@@ -15,7 +16,15 @@ class Game:
     def run(self):
         while True:
             menu = Menu(self.window)
-            menu.run()
-            pass
+            menu_return = menu.run()
+            
+            if menu_return in [MENU_OPTION[0], MENU_OPTION[1], MENU_OPTION[2]]:
+                level = Level(self.window, 'Level1', menu_return)
+                level_return = level.run()
+            elif menu_return == MENU_OPTION[4]:
+                pygame.quit() # Close window
+                quit() # End pygame
+            else:
+                pass
 
     
